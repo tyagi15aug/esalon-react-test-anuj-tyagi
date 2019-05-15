3190ca07bd073c75d22977e10dec802b0a129c36
tyagi15aug <tyagi.anuj@gmail.com>
2019-05-14 17:14:48 -0700

    Changed saga as API started working

--- src/saga/index.js
+++ src/saga/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { all , put, takeLatest } from "redux-saga/effects";
+import { all, call, put, takeLatest } from "redux-saga/effects";
 import { FETCH_API_DATA, dataReceived } from "../actions";
 import { appData } from "../response";
 
@@ -12,10 +12,10 @@ function fetchAPIData() {
 
 function* fetchEffectsSaga(action) {
   try {
-    // let { data } = yield call(fetchAPIData) || appData;
-    // yield put(dataReceived(data));
-    let { data } = appData;
-		yield put(dataReceived(data));
+    let { data } = yield call(fetchAPIData) || appData;
+    yield put(dataReceived(data.data));
+    // let { data } = appData;
+		// yield put(dataReceived(data));
   } catch (e) {
     console.log(e)
   }
